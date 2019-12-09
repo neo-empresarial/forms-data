50fd187fca8dfc3412375d180eee567678755a7c
Pedro Kretzschmar <pedro.kretz@gmail.com>
2019-12-09 12:37:58 -0300

    Add link to __fillable__

--- forms-data/models/form.py
+++ forms-data/models/form.py
@@ -3,7 +3,7 @@ from models import *
 class Form(Model):
   
   __table__ = "form"
-  __fillable__ = ["name"]
+  __fillable__ = ["name", "link"]
   __primary_key__ = "id"
   __incrementing__ = True
 
